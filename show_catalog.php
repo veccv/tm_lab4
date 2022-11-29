9f4af1c3f529884a2a87ccf928bf57d58c12d497
Jacek <jacdal000@pbs.edu.pl>
2022-11-29 10:57:38 +0100

    Added showing and exploring catalogs

--- show_catalog.php
+++ show_catalog.php
@@ -8,26 +8,9 @@ if (!isset($_SESSION['loggedin']))
 }
 
 $user = $_SESSION['user'];
-
-
-$auth_file = file_get_contents('auth.txt');
-$lines = explode("\n", $auth_file);
-$host = substr($lines[0], 5, -1);
-$username = substr($lines[1], 9, -1);
-$password = substr($lines[2], 9, -1);
-$database = substr($lines[3], 7);
-$link = mysqli_connect($host, $username, $password, $database);
-
-if(!$link) { echo"Błąd: ". mysqli_connect_errno()." ".mysqli_connect_error(); }
-mysqli_query($link, "SET NAMES 'utf8'");
-$result = mysqli_query($link, "SELECT * FROM break_ins WHERE login='$user' ORDER BY datetime DESC LIMIT 1");
-$rekord = mysqli_fetch_array($result);
-
-if ($rekord) {
-    echo "<div style='color: red'>Ostatnie błędne logowanie na to konto zostało wykonane z IP: " . $rekord[2] . ", data: " . $rekord[1] . "</div>";
-    echo "<br>";
-}
+$catalog = $_GET['catalog'];
 ?>
+
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl"> <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"> </head>
 <BODY>
@@ -46,19 +29,19 @@ echo "<table border='1'>
 <th style='padding: 15px'>Opcje</th>
 </tr>";
 
-$files = scandir($user);
+$files = scandir($user . "/" . $catalog);
 foreach ($files as $file) {
-if (strlen($file) > 1) {
-        if (!strpos($file, '.')) {
+    if (strlen($file) > 1) {
+        if ($file != "..") {
             echo "<tr>";
             echo "<td style='padding: 15px'>" . "</td>";
-            echo "<td style='padding: 15px'> <a href='show_catalog.php?catalog=$file'>" . $file . "</a></td>";
+            echo "<td style='padding: 15px'>" . $file . "</td>";
             echo "<td style='padding: 15px'>" . "</td>";
             echo "</tr>";
         } else {
             echo "<tr>";
             echo "<td style='padding: 15px'>" . "</td>";
-            echo "<td style='padding: 15px'>" . $file . "</td>";
+            echo "<td style='padding: 15px'> <a href='index4.php'>" . $file . "</a></td>";
             echo "<td style='padding: 15px'>" . "</td>";
             echo "</tr>";
         }
