2835efdb6b56f07aea19cb4acdc473f1f6680908
Jacek <jacdal000@pbs.edu.pl>
2022-11-29 13:53:14 +0100

    Added back to catalog with uploaded file

--- upload.php
+++ upload.php
@@ -1,9 +1,11 @@
 <?php
 session_start();
 $target_file = '';
+$next_site = 'index4.php';
 
 if (strlen($_POST['catalog']) > 0) {
     $target_file = $_SESSION['user'] . "/" . $_POST['catalog'] . '/' . basename($_FILES["fileToUpload"]["name"]);
+    $next_site = 'show_catalog.php?catalog=' . $_POST['catalog'];
 } else {
     $target_file = $_SESSION['user'] . "/" . basename($_FILES["fileToUpload"]["name"]);
 }
@@ -14,4 +16,4 @@ if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
     echo $_GET['catalog'];
     echo "Error uploading file.";
 }
-header('Location: index4.php');
+header('Location: ' . $next_site);
